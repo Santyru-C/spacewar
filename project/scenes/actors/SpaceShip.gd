980fcfac5aa151c98db44d0051dcd660739c360e
Santyru <santiago0988@hotmail.com>
2023-04-05 21:44:43 -0300

    add top down movement

--- project/scenes/actors/SpaceShip.gd
+++ project/scenes/actors/SpaceShip.gd
@@ -1,12 +1,13 @@
 extends KinematicBody2D
 
-var speed = 40
+var speed = 10
 var acceleration = Vector2(30, 0)
 # make the character accelerate faster when starting from 0
 
 var velocity = Vector2()
 var rotation_dir = 0
 var rotation_speed = 1
+var friction = Vector2(1, 0)
 
 	#velocity = transform.y * acceleration
 func get_input():
@@ -20,11 +21,15 @@ func _physics_process(delta):
 	get_input()
 	rotation += rotation_dir * rotation_speed * delta
 	if Input.is_action_pressed("ui_up"):
-		velocity = Vector2(0, -speed)
+		velocity -= transform.y * speed
+	
+	if velocity.length() > 0:
+		velocity -= friction.rotated(friction.angle_to(velocity))
+	velocity = velocity.limit_length(300)
 	#velocity += acceleration * delta
 	#print(velocity)
 	velocity = move_and_slide(velocity)
 
 
 func _on_Timer_timeout():
-	print(position)
+	print("friction", friction.angle_to(velocity))
