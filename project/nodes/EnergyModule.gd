227c411aa6314b4752c67ed94519f6b1c351c10f
Santyru <santiago0988@hotmail.com>
2023-04-20 19:28:47 -0300

    add basic functionality

--- project/nodes/EnergyModule.gd
+++ project/nodes/EnergyModule.gd
@@ -2,5 +2,12 @@ extends Node
 class_name EnergyModule
 
 export(int) var max_energy
-export(int) var energy_recovery_ammount
+export(float) var recovery_weight
+var current_energy = 0
 var recharge_enabled = true
+
+func substract_energy(amount):
+	max_energy -= amount
+	
+func recharge():
+	current_energy = lerp(current_energy, max_energy, 0.009)
