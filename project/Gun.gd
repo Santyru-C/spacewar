1baf631496f8507a1bffe1956881c5c11ebb4f63
Santyru <santiago0988@hotmail.com>
2023-04-20 20:34:18 -0300

    refactor shoot function

--- project/Gun.gd
+++ project/Gun.gd
@@ -1,10 +1,15 @@
 extends Node2D
 export(PackedScene) var current_proyectile_scene
+export(float) var fire_rate = 0.5
+var can_fire = true
 
-func _unhandled_input(event):
-	if Input.is_action_pressed("ui_accept"):
+func shoot():
+	if can_fire:
+		can_fire = false
 		var proyectile = current_proyectile_scene.instance()
 		get_parent().get_parent().add_child(proyectile)
 		proyectile.global_position = self.global_position
 		proyectile.rotation = get_parent().rotation - PI/2
 		proyectile.direction = Vector2.RIGHT.rotated(get_parent().rotation - PI/2)
+		yield(get_tree().create_timer(fire_rate), "timeout")
+		can_fire = true
