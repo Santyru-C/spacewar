44319808aa207f094a40fa9e9f943af7135d1651
Santyru <santiago0988@hotmail.com>
2023-04-20 19:33:57 -0300

    set default values

--- project/nodes/EnergyModule.gd
+++ project/nodes/EnergyModule.gd
@@ -1,8 +1,8 @@
 extends Node
 class_name EnergyModule
 
-export(int) var max_energy
-export(float) var recovery_weight
+export(int) var max_energy = 500
+export(float) var recovery_weight = 0.009
 var current_energy = 0
 var recharge_enabled = true
 
@@ -10,4 +10,4 @@ func substract_energy(amount):
 	max_energy -= amount
 	
 func recharge():
-	current_energy = lerp(current_energy, max_energy, 0.009)
+	current_energy = lerp(current_energy, max_energy, recovery_weight)
