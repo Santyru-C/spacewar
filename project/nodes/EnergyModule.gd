a8ecc07125ede4b3c390d28b3ac7b662fa804ce0
Santyru <santiago0988@hotmail.com>
2023-04-20 19:37:27 -0300

    add custom signals

--- project/nodes/EnergyModule.gd
+++ project/nodes/EnergyModule.gd
@@ -1,10 +1,14 @@
 extends Node
 class_name EnergyModule
 
+signal energy_depleted
+signal energy_down
+signal energy_recovering
+
 export(int) var max_energy = 500
 export(float) var recovery_weight = 0.009
 var current_energy = 0
-var recharge_enabled = true
+var recharge_enabled = false
 
 func substract_energy(amount):
 	max_energy -= amount
