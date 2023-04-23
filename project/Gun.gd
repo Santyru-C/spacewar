b81987a5f9097d43223335a47ae20ee95f4850a5
Santyru <santiago0988@hotmail.com>
2023-04-23 17:00:18 -0300

    refactor shoot function

--- project/Gun.gd
+++ project/Gun.gd
@@ -1,15 +1,36 @@
 extends Node2D
 export(PackedScene) var current_proyectile_scene
 export(float) var fire_rate = 0.5
+export(int) var max_charges = 3
+var current_charges = 0
 var can_fire = true
 
+signal charge_used
+signal charges_recoverd
+
+func instance_proyectile():
+	var proyectile = current_proyectile_scene.instance()
+	get_parent().get_parent().add_child(proyectile)
+	proyectile.global_position = self.global_position
+	proyectile.rotation = get_parent().rotation - PI/2
+	proyectile.direction = Vector2.RIGHT.rotated(get_parent().rotation - PI/2)
+	
+func set_fire_cooldown():
+	can_fire = false
+	yield(get_tree().create_timer(fire_rate), "timeout")
+	can_fire = true
+	
 func shoot():
-	if can_fire:
-		can_fire = false
-		var proyectile = current_proyectile_scene.instance()
-		get_parent().get_parent().add_child(proyectile)
-		proyectile.global_position = self.global_position
-		proyectile.rotation = get_parent().rotation - PI/2
-		proyectile.direction = Vector2.RIGHT.rotated(get_parent().rotation - PI/2)
-		yield(get_tree().create_timer(fire_rate), "timeout")
-		can_fire = true
+	if can_fire and current_charges > 0:
+		charges -= 1
+		emit_signal("charge_used")
+		set_fire_cooldown()
+		instance_proyectile()
+	
+	if charges == 0:
+		$RechargeTimer.start()
+
+
+func _on_RechargeTimer_timeout():
+	current_charges = max_charges
+	emit_signal("charges_recoverd")
