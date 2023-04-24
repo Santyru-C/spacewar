7e21b6dc285325324ec46fd5db9c4aa694f67ec3
Santyru <santiago0988@hotmail.com>
2023-04-24 20:46:48 -0300

    set base damage

--- project/scenes/Proyectile.gd
+++ project/scenes/Proyectile.gd
@@ -1,9 +1,12 @@
 extends Area2D
 
 export(int) var speed
-
+export(int) var damage_amount = 35
 var direction = Vector2()
-	
+
 func _physics_process(delta):
 	position += direction * speed * delta
 
+func _on_Proyectile_area_entered(area):
+	area.take_damage(damage_amount)
+	queue_free()
