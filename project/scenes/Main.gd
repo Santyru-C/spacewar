84f9c95a792aa6188a9934e9beaaa57bb77c2f48
Santyru <santiago0988@hotmail.com>
2023-04-26 20:13:58 -0300

    add instancing function

--- project/scenes/Main.gd
+++ project/scenes/Main.gd
@@ -1,13 +1,23 @@
 extends Node2D
 
-# add function to connect all player 1 and 2 signals
-func _ready():
+func connect_ship_signals_to_HUD(ship):
 	var HUD = $HUD
-	var energyModule = $SpaceShip/EnergyModule
-	var ship1Gun = $SpaceShip/Gun
+	var energyModule = ship.get_node("EnergyModule")
+	var gun = ship.get_node("Gun")
 	
 	energyModule.connect("energy_down", HUD, "on_energy_module_down")
 	energyModule.connect("recovering", HUD, "on_EnergyModule_recovering")
-	ship1Gun.connect("charge_used", HUD, "on_charge_used")
-	ship1Gun.connect("charges_recoverd", HUD, "on_charges_recovered")
-	ship1Gun.connect("update_max_charges", HUD, "on_max_charges_updated")
+	gun.connect("charge_used", HUD, "on_charge_used")
+	gun.connect("charges_recoverd", HUD, "on_charges_recovered")
+	gun.connect("update_max_charges", HUD, "on_max_charges_updated")
+	ship.connect("acelerating", HUD, "update_p1_speed")
+
+func instantiate_player(player):
+	var ship = load("res://scenes/actors/%s.tscn" % player)
+	var player_instance = ship.instance()
+	self.add_child(player_instance)
+	
+func _ready():
+	instantiate_player("SpaceShip1")
+	#var ship1 = $SpaceShip1
+	#connect_ship_signals_to_HUD(ship1)
