d60731484acd7934438d3f164fd783a09c99b396
Santyru <santiago0988@hotmail.com>
2023-04-26 19:35:06 -0300

    add display instancing function

--- project/HUD.gd
+++ project/HUD.gd
@@ -14,3 +14,24 @@ func on_charges_recovered():
 
 func on_max_charges_updated(amount):
 	$ChargesBar.max_value = amount
+
+func update_p1_speed(current_speed):
+	$SpeedLabel.text = str(current_speed)
+
+func set_p2_display():
+	var attributeDisplay_scene = load("res://scenes/InvertedAttributeDisplay.tscn")
+	var attributeDisplay = attributeDisplay_scene.instance()
+	$PlayerController2.add_child(attributeDisplay)
+	$PlayerController2/Label.queue_free()
+	attributeDisplay.rect_position.x -= attributeDisplay.rect_size.x + 20
+	
+func set_p1_display():
+	var attributeDisplay_scene = load("res://scenes/AttributeDisplay.tscn")
+	var attributeDisplay = attributeDisplay_scene.instance()
+	$PlayerController1/Label.queue_free()
+	$PlayerController1.add_child(attributeDisplay)	
+	
+func _ready():
+	set_p1_display()
+	set_p2_display()
+	
